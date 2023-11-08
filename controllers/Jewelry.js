3269e02defd8072c11a24c152bc3e5e31fc73b8b
JammulaSriLaxmiSathwika <143139467+JammulaSriLaxmiSathwika@users.noreply.github.com>
2023-11-08 15:43:28 -0600

    Update Jewelry.js

--- controllers/Jewelry.js
+++ controllers/Jewelry.js
@@ -1,17 +1,25 @@
 var Jewelry = require('../models/Jewelry');
 // List of all Jewelry
-exports.Jewelry_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Jewelry list');
-};
+exports.Jewelry_list = async function(req, res) {
+    try{
+    theJewelry = await Jewelry.find();
+    res.send(theJewelry);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 // for a specific Jewelry.
 exports.Jewelry_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Jewelry detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
+// Handle Jewelry create on POST.
 exports.Jewelry_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: Jewelry create POST');
 };
-// Handle Costume delete form on DELETE.
+// Handle jewelry delete form on DELETE.
 exports.Jewelry_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Jewelry delete DELETE ' + req.params.id);
 };
