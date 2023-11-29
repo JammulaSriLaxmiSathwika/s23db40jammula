04bc78dcb2b0062eb511bed267dad9b7780ede4e
JammulaSriLaxmiSathwika <143139467+JammulaSriLaxmiSathwika@users.noreply.github.com>
2023-11-28 21:25:17 -0600

    added pug files

--- routes/Jewelry.js
+++ routes/Jewelry.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const Jewelry_controlers= require('../controllers/Jewelry');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET Jewelry */
 router.get('/', Jewelry_controlers.Jewelry_view_all_Page );
 /* GET detail Jewelry page */
@@ -8,7 +14,7 @@ router.get('/detail', Jewelry_controlers.Jewelry_view_one_Page);
 /* GET create Jewelry page */
 router.get('/create', Jewelry_controlers.Jewelry_create_Page);
 /* GET create update page */
-router.get('/update', Jewelry_controlers.Jewelry_update_Page);
+router.get('/update',secured, Jewelry_controlers.Jewelry_update_Page);
 /* GET delete Jewelry page */
 router.get('/delete', Jewelry_controlers.Jewelry_delete_Page);
 module.exports = router;
