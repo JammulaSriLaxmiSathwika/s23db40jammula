f88b65ce2516977cddab3a922323c578ae623c7f
JammulaSriLaxmiSathwika <143139467+JammulaSriLaxmiSathwika@users.noreply.github.com>
2023-11-28 22:00:48 -0600

    Update Jewelry.js

--- models/Jewelry.js
+++ models/Jewelry.js
@@ -1,8 +1,20 @@
+const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const JewelrySchema = mongoose.Schema({
-type: String,
-material: String,
-price: Number
+type: {
+    type: String,
+    required: true
+},
+material: {
+    type: String,
+    required: true
+},
+price: {
+    type: Number,
+    required: true,
+    min: 100,
+    max: 4000
+}
 })
 module.exports = mongoose.model("Jewelry",
 JewelrySchema)
